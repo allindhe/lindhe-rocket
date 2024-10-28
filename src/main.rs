fee810402362eb60a1e98229c1ac46d8b18f6506
Alexander Lindhe <allindhe@gmail.com>
2024-10-27 21:20:27 -0500

    First commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use rocket::{
+    fs::{relative, FileServer},
+    get, launch, routes,
+};
+use rocket_dyn_templates::{context, Template};
+
+#[get("/")]
+async fn index() -> Template {
+    Template::render(
+        "index",
+        context! {
+            foo: 123,
+        },
+    )
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .mount("/", FileServer::from(relative!("/static")))
+}
